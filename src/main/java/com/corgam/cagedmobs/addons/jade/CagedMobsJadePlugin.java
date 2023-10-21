bf74f0fe258921de04d62a65e94b75a93a760310
Emil Balicki <emil.balicki2@gmail.com>
2023-10-21 16:15:24 +0200

    Cleaned up Jade compatability

--- src/main/java/com/corgam/cagedmobs/addons/jade/CagedMobsJadePlugin.java
+++ src/main/java/com/corgam/cagedmobs/addons/jade/CagedMobsJadePlugin.java
@@ -1,7 +1,9 @@
 package com.corgam.cagedmobs.addons.jade;
 
 import com.corgam.cagedmobs.blocks.mob_cage.MobCageBlock;
+import com.corgam.cagedmobs.blocks.mob_cage.MobCageBlockEntity;
 import snownee.jade.api.IWailaClientRegistration;
+import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
 import snownee.jade.api.WailaPlugin;
 
@@ -11,5 +13,10 @@ public class CagedMobsJadePlugin implements IWailaPlugin {
     @Override
     public void registerClient(IWailaClientRegistration registration) {
         registration.registerBlockComponent(new CagedMobsComponentProvider(), MobCageBlock.class);
+    }
+
+    @Override
+    public void register(IWailaCommonRegistration registration){
+        registration.registerItemStorage(new HideContainerItemsProvider(), MobCageBlockEntity.class);
     }
 }
