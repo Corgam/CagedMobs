c5852e5a690802047a399d69c23dec29d0397d84
Emil Balicki <emil.balicki2@gmail.com>
2023-12-11 17:39:29 +0100

    Update CagedBlocks.java

--- src/main/java/com/corgam/cagedmobs/registers/CagedBlocks.java
+++ src/main/java/com/corgam/cagedmobs/registers/CagedBlocks.java
@@ -21,5 +21,5 @@ public class CagedBlocks {
     public final static RegistryObject<Block> HOPPING_MOB_CAGE = CAGED_BLOCKS_REGISTER.register("hopping_mob_cage", () -> new HoppingMobCageBlock(Block.Properties.of(Material.METAL, MaterialColor.METAL).sound(SoundType.METAL).strength(5.0F, 6.0F).requiresCorrectToolForDrops()));
     // BLOCKS
     public final static RegistryObject<Block> STAR_INFUSED_NETHERITE_BLOCK = CAGED_BLOCKS_REGISTER.register("star_infused_netherite_block", () -> new StarInfusedNetheriteBlock(Block.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).sound(SoundType.NETHERITE_BLOCK).requiresCorrectToolForDrops().strength(50.0F, 1200.0F)));
-    public final static RegistryObject<Block> CRYSTALLIZED_EXPERIENCE_BLOCK = CAGED_BLOCKS_REGISTER.register("crystallized_experience_block", () -> new CrystallizedExperienceBlock(Block.Properties.of(Material.FROGSPAWN, MaterialColor.COLOR_LIGHT_GREEN).sound(SoundType.SLIME_BLOCK).noOcclusion()));
+    public final static RegistryObject<Block> CRYSTALLIZED_EXPERIENCE_BLOCK = CAGED_BLOCKS_REGISTER.register("crystallized_experience_block", () -> new CrystallizedExperienceBlock(Block.Properties.of(Material.CLAY, MaterialColor.COLOR_LIGHT_GREEN).sound(SoundType.SLIME_BLOCK).noOcclusion()));
 }
