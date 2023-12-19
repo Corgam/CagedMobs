49b4cd504e8c72bbce456954700d1fa8de87d5e2
Emil Balicki <emil.balicki2@gmail.com>
2023-12-19 21:20:48 +0100

    Reimplemented Jade support

--- src/main/java/com/corgam/cagedmobs/addons/jade/CagedMobsComponentProvider.java
+++ src/main/java/com/corgam/cagedmobs/addons/jade/CagedMobsComponentProvider.java
@@ -1,77 +1,77 @@
-//package com.corgam.cagedmobs.addons.jade;
-//
-//import com.corgam.cagedmobs.CagedMobs;
-//import com.corgam.cagedmobs.blocks.mob_cage.MobCageBlockEntity;
-//import net.minecraft.ChatFormatting;
-//import net.minecraft.network.chat.Component;
-//import net.minecraft.resources.ResourceLocation;
-//import net.minecraft.world.entity.EntityType;
-//import net.minecraft.world.item.ItemStack;
-//import snownee.jade.api.BlockAccessor;
-//import snownee.jade.api.IBlockComponentProvider;
-//import snownee.jade.api.ITooltip;
-//import snownee.jade.api.config.IPluginConfig;
-//import snownee.jade.api.ui.BoxStyle;
-//import snownee.jade.api.ui.IElement;
-//import snownee.jade.api.ui.IElementHelper;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class CagedMobsComponentProvider implements IBlockComponentProvider {
-//
-//    @Override
-//    public void appendTooltip(ITooltip tooltip, BlockAccessor blockAccessor, IPluginConfig pluginConfig) {
-//        if(!(blockAccessor.getBlockEntity() instanceof MobCageBlockEntity tile)){
-//            return;
-//        }
-//        IElementHelper helper = tooltip.getElementHelper();
-//        // Add growth progress
-//        if(tile.hasEntity() && tile.hasEnvironment()){
-//            tooltip.add(helper.progress(tile.getGrowthPercentage(),
-//                    Component.literal(String.format("%3.0f%%", tile.getGrowthPercentage() * 100)),
-//                    helper.progressStyle().color(0xff44AA44, 0xff44AA44),
-//                    BoxStyle.GradientBorder.TRANSPARENT,
-//                    true
-//            ));
-//        }
-//        // Add Environment
-//        if(tile.hasEnvironment()){
-//            ItemStack representation = tile.getEnvironmentItemStack();
-//            if(representation != null){
-//                tooltip.add(Component.translatable("JADE.tooltip.cagedmobs.cage.environment"));
-//                tooltip.add(List.of(
-//                        helper.item(representation, 1.0F),
-//                        helper.text(representation.getHoverName())));
-//            }
-//        }
-//        // Add Entity
-//        if(tile.hasEntity()){
-//            EntityType<?> representation = tile.getEntityType();
-//            if(representation != null){
-//                tooltip.add(Component.literal(
-//                        Component.translatable("JADE.tooltip.cagedmobs.cage.entity").withStyle(ChatFormatting.GRAY).getString() +
-//                                Component.translatable(representation.getDescriptionId()).withStyle(ChatFormatting.GRAY).getString()));
-//            }
-//        }
-//        // Add Upgrades
-//        if(tile.hasAnyUpgrades()){
-//            // Add Upgrade text
-//            tooltip.add(Component.translatable("TOP.tooltip.cagedmobs.cage.upgrades"));
-//            // Iterate through upgrades
-//            List<IElement> upgrades = new ArrayList<>();
-//            for(ItemStack upgrade : tile.getUpgradesAsItemStacks()){
-//                if(!upgrade.isEmpty()){
-//                    upgrades.add(helper.item(upgrade));
-//                }
-//            }
-//            // Render a list of upgrades
-//            tooltip.add(upgrades);
-//        }
-//    }
-//
-//    @Override
-//    public ResourceLocation getUid() {
-//        return new ResourceLocation(CagedMobs.MOD_ID, "cagedmobs_jade");
-//    }
-//}
+package com.corgam.cagedmobs.addons.jade;
+
+import com.corgam.cagedmobs.CagedMobs;
+import com.corgam.cagedmobs.blocks.mob_cage.MobCageBlockEntity;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.ItemStack;
+import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.IBlockComponentProvider;
+import snownee.jade.api.ITooltip;
+import snownee.jade.api.config.IPluginConfig;
+import snownee.jade.api.ui.BoxStyle;
+import snownee.jade.api.ui.IElement;
+import snownee.jade.api.ui.IElementHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CagedMobsComponentProvider implements IBlockComponentProvider {
+
+    @Override
+    public void appendTooltip(ITooltip tooltip, BlockAccessor blockAccessor, IPluginConfig pluginConfig) {
+        if(!(blockAccessor.getBlockEntity() instanceof MobCageBlockEntity tile)){
+            return;
+        }
+        IElementHelper helper = tooltip.getElementHelper();
+        // Add growth progress
+        if(tile.hasEntity() && tile.hasEnvironment()){
+            tooltip.add(helper.progress(tile.getGrowthPercentage(),
+                    Component.literal(String.format("%3.0f%%", tile.getGrowthPercentage() * 100)),
+                    helper.progressStyle().color(0xff44AA44, 0xff44AA44),
+                    BoxStyle.GradientBorder.DEFAULT_NESTED_BOX,
+                    true
+            ));
+        }
+        // Add Environment
+        if(tile.hasEnvironment()){
+            ItemStack representation = tile.getEnvironmentItemStack();
+            if(representation != null){
+                tooltip.add(Component.translatable("JADE.tooltip.cagedmobs.cage.environment"));
+                tooltip.add(List.of(
+                        helper.item(representation, 1.0F),
+                        helper.text(representation.getHoverName())));
+            }
+        }
+        // Add Entity
+        if(tile.hasEntity()){
+            EntityType<?> representation = tile.getEntityType();
+            if(representation != null){
+                tooltip.add(Component.literal(
+                        Component.translatable("JADE.tooltip.cagedmobs.cage.entity").withStyle(ChatFormatting.GRAY).getString() +
+                                Component.translatable(representation.getDescriptionId()).withStyle(ChatFormatting.GRAY).getString()));
+            }
+        }
+        // Add Upgrades
+        if(tile.hasAnyUpgrades()){
+            // Add Upgrade text
+            tooltip.add(Component.translatable("TOP.tooltip.cagedmobs.cage.upgrades"));
+            // Iterate through upgrades
+            List<IElement> upgrades = new ArrayList<>();
+            for(ItemStack upgrade : tile.getUpgradesAsItemStacks()){
+                if(!upgrade.isEmpty()){
+                    upgrades.add(helper.item(upgrade));
+                }
+            }
+            // Render a list of upgrades
+            tooltip.add(upgrades);
+        }
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return new ResourceLocation(CagedMobs.MOD_ID, "cagedmobs_jade");
+    }
+}
--- src/main/java/com/corgam/cagedmobs/addons/jade/HideContainerItemsProvider.java
+++ src/main/java/com/corgam/cagedmobs/addons/jade/HideContainerItemsProvider.java
@@ -1,29 +1,29 @@
-//package com.corgam.cagedmobs.addons.jade;
-//
-//import com.corgam.cagedmobs.CagedMobs;
-//import com.corgam.cagedmobs.blocks.mob_cage.MobCageBlockEntity;
-//import net.minecraft.resources.ResourceLocation;
-//import net.minecraft.world.item.ItemStack;
-//import org.jetbrains.annotations.Nullable;
-//import snownee.jade.api.Accessor;
-//import snownee.jade.api.view.IServerExtensionProvider;
-//import snownee.jade.api.view.ViewGroup;
-//
-//import java.util.List;
-//
-//public class HideContainerItemsProvider implements IServerExtensionProvider<MobCageBlockEntity, ItemStack> {
-//
-//    public static final ResourceLocation UID = new ResourceLocation(CagedMobs.MOD_ID,"hide_container_items");
-//
-//    @Override
-//    public @Nullable List<ViewGroup<ItemStack>> getGroups(Accessor<?> accessor, MobCageBlockEntity mobCageBlockEntity) {
-//        return List.of();
-//    }
-//
-//    @Override
-//    public ResourceLocation getUid() {
-//        return UID;
-//    }
-//
-//
-//}
+package com.corgam.cagedmobs.addons.jade;
+
+import com.corgam.cagedmobs.CagedMobs;
+import com.corgam.cagedmobs.blocks.mob_cage.MobCageBlockEntity;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import snownee.jade.api.Accessor;
+import snownee.jade.api.view.IServerExtensionProvider;
+import snownee.jade.api.view.ViewGroup;
+
+import java.util.List;
+
+public class HideContainerItemsProvider implements IServerExtensionProvider<MobCageBlockEntity, ItemStack> {
+
+    public static final ResourceLocation UID = new ResourceLocation(CagedMobs.MOD_ID,"hide_container_items");
+
+    @Override
+    public @Nullable List<ViewGroup<ItemStack>> getGroups(Accessor<?> accessor, MobCageBlockEntity mobCageBlockEntity) {
+        return List.of();
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return UID;
+    }
+
+
+}
